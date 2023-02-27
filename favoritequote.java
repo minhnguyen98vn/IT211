cbd1842fea525b395de1de4264dc5e084138512c
minhnguyen98vn <63829855+minhnguyen98vn@users.noreply.github.com>
2023-02-27 15:01:23 -0800

    Add files via upload

---
+++ favoritequote.java
@@ -0,0 +1,12 @@
+/* 
+ Favorite Quote
+
+*/
+
+class FavoriteQuote {
+    // A Java program begins with a call to main()
+    public static void main(String[] args) {
+        String output = "My name is Minh Nguyen";
+        System.out.println(output);
+    }
+}
