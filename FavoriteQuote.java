670fd6c962ae7ceeaa0b5007fc96ba75ee5405f0
minhnguyen98vn <63829855+minhnguyen98vn@users.noreply.github.com>
2023-02-27 14:47:17 -0800

    Update FavoriteQuote.java

--- FavoriteQuote.java
+++ FavoriteQuote.java
@@ -1,7 +1,7 @@
 class FirstProgram {
     // A Java program begins with a call to main()
     public static void main(String[] args) {
-        String output = "My name is BC";
+        String output = "My name is Minh Nguyen";
         System.out.println(output);
     }
 }
